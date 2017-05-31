8b4bd6dc3936aafdfeb335cc1c6e6f8140d61fa3
Alvaro Cabrera <pateketrueke@gmail.com>
2017-05-31 14:23:56 -0500

    Fixed blocked impl

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,6 @@
 $iframe = document.querySelector('.js-canvas iframe');
-$target = document.querySelector('.js-target');
+$close = document.querySelector('.js-close');
+$body = document.body;
 
 let isOpen;
 
@@ -8,51 +9,36 @@ let isOpen;
   node.addEventListener('click', e => {
     e.preventDefault();
 
-    $target.classList.add('active');
+    $body.classList.add('blocked');
 
     $iframe.src = e.currentTarget.href;
     $iframe.onload = () => {
       setTimeout(() => {
-        $target.classList.add('display');
         $iframe.classList.add('active');
       }, 300);
     };
-
-    document.body.style.overflow = 'hidden';
 
     isOpen = true;
   });
 });
 
-[].slice.call(document.querySelectorAll('a.js-close'))
-.forEach(node => {
-  node.addEventListener('click', e => {
-    e.preventDefault();
-    close();
-  });
+$close.addEventListener('click', e => {
+  e.preventDefault();
+  close();
 });
 
 function close() {
   if (isOpen) {
-    $target.classList.remove('display');
+    $body.classList.remove('blocked');
 
     setTimeout(() => {
       $iframe.classList.remove('active');
-      $target.classList.remove('active');
       $iframe.src = 'about:blank';
     }, 100);
-
-    document.body.style.overflow = '';
 
     isOpen = false;
   }
 }
-
-$target.addEventListener('click', e => {
-  if (e.target === $target) {
-    close();
-  }
-});
 
 window.addEventListener('keyup', e => {
   if (e.keyCode === 27) {
