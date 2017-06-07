c9e36ab924579ef2f4a5407d65fce672c7c1a27c
Alvaro Cabrera <pateketrueke@gmail.com>
2017-06-07 04:42:43 -0500

    Fixed help

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,37 @@
+[].slice.call(document.querySelectorAll('ul.js-tabs'))
+.forEach(srcNode => {
+  const targetNode = srcNode.nextElementSibling;
+  const tabList = [].slice.call(srcNode.querySelectorAll('li'));
+  const tabBodiesList = [].slice.call(targetNode.querySelectorAll('li'));
+  const count = tabList.length;
+
+  let key = 0;
+
+  tabList[key].classList.add('is-active');
+  tabBodiesList[key].classList.add('is-active');
+
+  srcNode.addEventListener('click', e => {
+    if (e.target.tagName === 'A') {
+      e.preventDefault();
+
+      if (key >= 0) {
+        tabList[key].classList.remove('is-active');
+        tabBodiesList[key].classList.remove('is-active');
+      }
+
+      for (let x = 0; x < count; x += 1) {
+        if (tabList[x] === e.target.parentNode) {
+          key = x;
+          tabList[x].classList.add('is-active');
+          tabBodiesList[x].classList.add('is-active');
+          break;
+        }
+      }
+    }
+  });
+});
+
+
 const $iframe = document.querySelector('.js-canvas iframe');
 const $close = document.querySelector('.js-close');
 const $head = document.querySelector('.header');
